f7bb3f1307bfba766c24f90c7981dc723fd83b50
Jovan L <327527689@qq.com>
2022-10-08 00:11:01 +0800

    added the first solution of leetcode 0234

---
+++ leetcode/leetcode0234/Solution.java
@@ -0,0 +1,48 @@
+package leetcode.leetcode0234;
+
+import org.junit.jupiter.api.Test;
+
+public class Solution {
+
+    @Test
+    void test() {
+        isPalindrome(null);
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        ListNode left = head;
+        ListNode right = reverse(slow);
+        while (right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+        ListNode next = head;
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+}
