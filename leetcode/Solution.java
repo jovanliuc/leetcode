089f60c713495c2be7ccee6435e06e5fe70d601d
Jovan L <327527689@qq.com>
2022-10-09 00:08:11 +0800

    added the first solution of leetcode of 0048

---
+++ leetcode/Solution.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+public class Solution {
+
+    @Test
+    void test() {
+        rotate(null);
+    }
+
+    public void rotate(int[][] matrix) {
+        int length = matrix.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                matrix[i][j] = matrix[i][j]^matrix[j][i];
+                matrix[j][i] = matrix[i][j]^matrix[j][i];
+                matrix[i][j] = matrix[i][j]^matrix[j][i];
+            }
+        }
+
+        for (int[] row : matrix) {
+            int i = 0;
+            int j = row.length - 1;
+            while (i < j) {
+                row[i] = row[i]^row[j];
+                row[j] = row[i]^row[j];
+                row[i] = row[i]^row[j];
+                i++;
+                j--;
+            }
+        }
+    }
+}
