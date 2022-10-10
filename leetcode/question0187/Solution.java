ed4dedbac633873e5297af7cae9a276663d4e388
Jovan L <327527689@qq.com>
2022-10-10 15:02:47 +0800

    added the first solution of leetcode 0187

--- leetcode/question0187/Solution.java
+++ leetcode/question0187/Solution.java
@@ -1,2 +1,38 @@
-package leetcode.question0187;public class Solution {
+package leetcode.question0187;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+
+    @Test
+    void test() {
+        findRepeatedDnaSequences("AAAAAAAAAAAAA");
+    }
+
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> result = new ArrayList<>();
+        if (s == null || s.length() == 0) {
+            return result;
+        }
+
+        Set<String> repeatedStrSet = new HashSet<>();
+        Set<String> resultSet = new HashSet<>();
+        // Left is opening, and the right is closed
+        for (int i = 0; i + 10 <= s.length(); i++) {
+
+            String subStr = s.substring(i, i + 10);
+            if (repeatedStrSet.contains(subStr)) {
+                resultSet.add(subStr);
+            } else {
+                repeatedStrSet.add(subStr);
+            }
+        }
+
+        return new ArrayList<>(resultSet);
+    }
 }
