178e15dece9b8f754521b82c32bac130da761c81
Jovan L <327527689@qq.com>
2022-10-11 22:36:22 +0800

    added the second solution of leetcode of 0226

---
+++ leetcode/question0226/Solution1.java
@@ -0,0 +1,33 @@
+package leetcode.question0226;
+
+import org.junit.jupiter.api.Test;
+
+public class Solution1 {
+
+    @Test
+    void test() {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        invertTree(root);
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+}
