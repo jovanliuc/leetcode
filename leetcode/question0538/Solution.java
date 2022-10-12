cf71bafbd7d361bd68bd6d310dd0f6ac58fa4c6c
Jovan L <327527689@qq.com>
2022-10-12 23:52:10 +0800

    added the first solution of leetcode 0538

---
+++ leetcode/question0538/Solution.java
@@ -0,0 +1,35 @@
+package leetcode.question0538;
+
+import org.junit.jupiter.api.Test;
+
+public class Solution {
+
+    private int sum = 0;
+
+    @Test
+    void test() {
+        TreeNode root = new TreeNode(50);
+        root.left = new TreeNode(20);
+        root.right = new TreeNode(80);
+        root.left.left = new TreeNode(10);
+        root.left.right =  new TreeNode(30);
+        root.right.left = new TreeNode(60);
+        root.right.right = new TreeNode(90);
+        convertBST(root);
+    }
+
+    public TreeNode convertBST(TreeNode root) {
+        traverse(root);
+        return root;
+    }
+
+    void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        traverse(root.right);
+        root.val = sum += root.val;
+        traverse(root.left);
+    }
+}
