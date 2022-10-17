2430fb84184d29c6f869c3fc1d02756261bd77cd
Jovan L <327527689@qq.com>
2022-10-18 00:15:39 +0800

    added the first solution of leetcode 0112

---
+++ leetcode/question0122/Solution.java
@@ -0,0 +1,32 @@
+package leetcode.question0122;
+
+import org.junit.jupiter.api.Test;
+
+public class Solution {
+
+    @Test
+    void test() {
+        maxProfit(new int[]{7,1,5,3,6,4});
+    }
+
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+                continue;
+            }
+
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+
+        return dp[n - 1][0];
+    }
+}
