68d7070512eb7a1599c31c1cdf7a08aab4a0bd50
Jovan L <327527689@qq.com>
2022-10-21 00:42:22 +0800

    added the first solution of leetcode 0617

---
+++ leetcode/question0617/Solution.java
@@ -0,0 +1,27 @@
+package leetcode.question0617;
+
+import org.junit.jupiter.api.Test;
+
+public class Solution {
+
+    @Test
+    void test() {
+        mergeTrees(null, null);
+    }
+
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+
+        if (root2 == null) {
+            return root1;
+        }
+
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+
+        return root1;
+    }
+}
