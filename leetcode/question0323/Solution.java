da27d6ba8b432ba09741ec9e310f346b7b057eab
Jovan L <327527689@qq.com>
2022-10-21 12:09:56 +0800

    added the first solution of leetcode 0323

---
+++ leetcode/question0323/Solution.java
@@ -0,0 +1,28 @@
+package leetcode.question0323;
+
+import org.junit.jupiter.api.Test;
+
+public class Solution {
+
+    @Test
+    void test() {
+        int n = 5;
+        int[][] edges = {
+            {0,2},
+            {1,2},
+            {2,3},
+            {3, 4}
+        };
+        countComponents(n, edges);
+    }
+
+    public int countComponents(int n, int[][] edges) {
+        UF uf = new UF(n);
+        for (int[] edge : edges) {
+            int v = edge[0];
+            int w = edge[1];
+            uf.union(v, w);
+        }
+        return uf.count();
+    }
+}
---
+++ leetcode/question0323/UF.java
@@ -0,0 +1,42 @@
+package leetcode.question0323;
+
+public class UF {
+
+    private int count;
+    private int[] parent;
+
+    public UF(int n) {
+        this.count = n;
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) {
+            return;
+        }
+        parent[rootQ] = rootP;
+        count--;
+    }
+
+    public boolean connected (int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        return rootP == rootQ;
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public int count() {
+        return this.count;
+    }
+}
