7a3c33387a504d26513cf23267f2e3e9c308048c
Jovan L <327527689@qq.com>
2022-10-25 11:52:21 +0800

    added the first solution of leetcode 2235

---
+++ leetcode/question2235/Solution.java
@@ -0,0 +1,15 @@
+package leetcode.question2235;
+
+import org.junit.jupiter.api.Test;
+
+public class Solution {
+
+    @Test
+    void test() {
+        sum(1, 1);
+    }
+
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
