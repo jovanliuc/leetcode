0a0f689d0042d1225df3459011ad4720b3d0f0d8
Jovan L <327527689@qq.com>
2022-10-28 23:57:12 +0800

    added the first solution of leetcode 0146

---
+++ leetcode/question0146/LRUCache.java
@@ -0,0 +1,43 @@
+package leetcode.question0146;
+
+import java.util.LinkedHashMap;
+
+public class LRUCache {
+
+    private int capacity;
+    private LinkedHashMap<Integer, Integer> cache;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new LinkedHashMap<>();
+    }
+
+    public int get(int key) {
+        if (!cache.containsKey(key)) {
+            return -1;
+        }
+        makeRecently(key);
+        return cache.get(key);
+    }
+
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            cache.put(key, value);
+            makeRecently(key);
+            return;
+        }
+
+        if (cache.size() >= this.capacity) {
+            int oldestKey = cache.keySet().iterator().next();
+            cache.remove(oldestKey);
+        }
+
+        cache.put(key, value);
+    }
+
+    private void makeRecently(int key) {
+        int val = cache.get(key);
+        cache.remove(key);
+        cache.put(key, val);
+    }
+}
